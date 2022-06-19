7cd7e3f2b6ba403b6d8b7f07ed24303521978dd6
gscoming <gscoming@outlook.com>
2022-06-19 20:16:32 +0800

    上传第一个版本

---
+++ 01点亮LED/led light.c
@@ -0,0 +1,24 @@
+# include "reg52.h"
+sbit led0 = P2^0;
+sbit led1 = P2^1;
+sbit led2 = P2^2;
+sbit led3 = P2^3;
+sbit led4 = P2^4;
+sbit led5 = P2^5;
+sbit led6 = P2^6;
+sbit led7 = P2^7;
+void main()
+{
+		while (1)
+		{
+			led0 = 0;
+			led1 = 0;
+			led2 = 0;
+			led3 = 0;
+			led4 = 0;
+			led5 = 0;
+			led6 = 0;
+			led7 = 0;
+
+		}
+}
